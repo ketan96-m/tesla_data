9568c6bda7671ede8fdbcd6e5108d9e01d238a77
Ketan Mahajan <ketanbm2@illinois.edu>
2023-05-16 00:44:20 -0500

    gunicorn

--- megapack/settings.py
+++ megapack/settings.py
@@ -93,6 +93,12 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'errors',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
